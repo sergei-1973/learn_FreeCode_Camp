7c7eac3059b54a13c234ab8e7e60fd45462ea0a0
sergei-1973 <baragozik00@gmail.com>
2025-11-29 16:31:20 +0300

    29.11.2025 desktoppc

--- js/free_dom.js
+++ js/free_dom.js
@@ -1,7 +1,9 @@
 "use strict";
-const container = document.getElementById("container");
-const img = document.createElement("img");
-img.src = "https://cdn.freecodecamp.org/curriculum/cat-photo-app/lasagna.jpg";
-img.alt = "A slice of lasagna on a plate.";
-container.appendChild(img);
-console.log(container);
+// const container = document.getElementById("container");
+// const img = document.createElement("img");
+// img.src = "https://cdn.freecodecamp.org/curriculum/cat-photo-app/lasagna.jpg";
+// img.alt = "A slice of lasagna on a plate.";
+// container.appendChild(img);
+// console.log(container);
+const desertList = document.getElementById("desserts");
+console.log(desertList);
