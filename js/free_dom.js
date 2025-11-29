25e97241b95fc685d744bc9c50d5a5cf852920b9
sergei-1973 <baragozik00@gmail.com>
2025-11-29 12:44:26 +0300

    29.11.2025 create dom

---
+++ js/free_dom.js
@@ -0,0 +1,7 @@
+"use strict";
+const container = document.getElementById("container");
+const img = document.createElement("img");
+img.src = "https://cdn.freecodecamp.org/curriculum/cat-photo-app/lasagna.jpg";
+img.alt = "A slice of lasagna on a plate.";
+container.appendChild(img);
+console.log(container);
