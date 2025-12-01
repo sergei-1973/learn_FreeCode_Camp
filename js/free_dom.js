a77e13035f33b1966c77d991863a4a372cd19c35
sergei-1973 <baragozik00@gmail.com>
2025-12-02 02:12:09 +0300

    02.12.2025 noobook

--- js/free_dom.js
+++ js/free_dom.js
@@ -43,3 +43,62 @@
 //   console.log("DOMContentLoaded has already fired");
 //   changeImg();
 // }
+
+
+// ========АНИМАЦИЯ===========================
+// const rect = document.getElementById('rect')
+// let position = 0
+
+// function update() {
+//     rect.style.left = position + 'px'
+//     position += 5
+
+//     if (position > window.innerWidth) {
+//         position = -rect.offsetWidth
+//     }
+// }
+// function animate() {
+//     update()
+
+//     requestAnimationFrame(animate)
+// }
+// requestAnimationFrame(animate)
+// const square = document.querySelector('#square');
+// const playBtn = document.querySelector("#playBtn");
+// const pauseBtn = document.querySelector("#pauseBtn");
+
+
+// const animation = square.animate(
+//     [{ transform: "translateX(0px)" }, { transform: "translateX(200px)" }],
+//     {
+//         duration: 5000,
+//         // iteration: Infinity,
+//         direction: 'alternate',
+//         easing: 'ease-in-out'
+//     }
+// )
+// // Set the onfinish property to log a message when the animation ends
+// animation.onfinish = () => {
+//     console.log("Animation finished!");
+// };
+
+// // Play the animation when the "Play" button is clicked
+// playBtn.addEventListener("click", () => {
+//     animation.play();
+//     console.log("You start the animation");
+// });
+
+// // Pause the animation when the "Pause" button is clicked
+// pauseBtn.addEventListener("click", () => {
+//     animation.pause();
+//     console.log("You pause the animation");
+// });
+
+// const canvas = document.getElementById("my-canvas");
+// canvas.width = 300;
+// canvas.heigth = 70;
+// const ctx = canvas.getContext('2d');
+// ctx.fillStyle = 'crimson';
+// // ctx.fillRect(1, 1, 150, 100)
+// ctx.font = '30px Arial'
+// ctx.fillText('Hello HTML Canvas!', 1, 50)
